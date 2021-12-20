c8a49df14701c5fabc14af18d8ee827cfa90d60c
leguan <59799222+Leguan16@users.noreply.github.com>
2021-12-20 11:57:52 +0100

    jdbcIndividuell v5

--- src/main/java/at/noah/jdbcIndividuell/fx/Controller.java
+++ src/main/java/at/noah/jdbcIndividuell/fx/Controller.java
@@ -1,14 +1,36 @@
 package at.noah.jdbcIndividuell.fx;
 
+import at.noah.jdbcIndividuell.domain.Device;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    private TableView<Device> tableViewDevices;
+
+    @FXML
+    private Button buttonRemove;
+
+    @FXML
+    private Button buttonEdit;
+
+    @FXML
+    private Button buttonChangeOwner;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        tableViewDevices.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
+        buttonEdit.setOnAction(actionEvent -> {
+
+        });
     }
+
 }
